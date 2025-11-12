44b888494da0efbbc0bc633235a99f150e40f0bf
bbktr <k.baba.h7.biz@gmail.com>
2025-11-12 22:11:32 +0900

    aboutとblogページを追加

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,16 @@
+type Params = {
+    params: Promise<{
+        id:string
+    }>
+}
+
+export default async function page({params}:Params) {
+// console.log(params)
+const { id } = await params
+
+  return (
+    <div>
+        ブログID:{id}
+    </div>
+  )
+}
