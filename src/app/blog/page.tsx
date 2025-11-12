04d19b52b08d99847a9654779c125451b9229705
bbktr <k.baba.h7.biz@gmai.com>
2025-11-12 23:40:13 +0900

    NOTFOUND・ローディング・エラーページ追加＆apiフォルダ作成

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,28 @@
+// ダミーデータ
+const articles = [
+    {id:"1",title:"タイトル1"},
+     {id:"2",title:"タイトル2"},
+      {id:"3",title:"タイトル3"}
+]
+
+// ３秒待機
+async function fetchArticles(){
+    await new Promise((resolve)=> setTimeout(resolve,3000))
+    // throw new Error('エラーが発生')
+    return articles
+}
+
+export default async function BlogPage() {
+        const articles = await fetchArticles()
+  return (
+    <div>
+      <ul>
+        { articles.map((articles)=>(
+            <li key={articles.id}>
+                title:{articles.title}
+            </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
