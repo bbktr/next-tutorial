021b9a5c7928357f0fc609fbd1adf6091f71b5cf
bbktr <k.baba.h7.biz@gmail.com>
2025-11-13 15:42:10 +0900

    rsc,rcc直下page.tsx内にLinkを追加

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,8 +1,13 @@
+import ClientComponent from "@/components/ClientComponent"
+import Link from "next/link"
+
 export default function ServerComponent() {
     console.log('Server')
     return (
         <div>
             サーバー
+            <ClientComponent />
+            <Link href="/about">About</Link>
         </div>
     )
 }
