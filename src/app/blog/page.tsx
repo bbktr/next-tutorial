cc1ae2bccfc4bf139765c678633b1a8db072619a
bbktr <k.baba.h7.biz@gmail.com>
2025-11-13 14:57:15 +0900

    動作確認用にrcc&rscを作成

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,27 +7,27 @@ export const metadata: Metadata = {
 
 // ダミーデータ
 const articles = [
-    {id:"1",title:"タイトル1"},
-     {id:"2",title:"タイトル2"},
-      {id:"3",title:"タイトル3"}
+  { id: "1", title: "タイトル1" },
+  { id: "2", title: "タイトル2" },
+  { id: "3", title: "タイトル3" }
 ]
 
 // ３秒待機
-async function fetchArticles(){
-    await new Promise((resolve)=> setTimeout(resolve,3000))
-    // throw new Error('エラーが発生')
-    return articles
+async function fetchArticles() {
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  // throw new Error('エラーが発生')
+  return articles
 }
 
 export default async function BlogPage() {
-        const articles = await fetchArticles()
+  const articles = await fetchArticles()
   return (
     <div>
       <ul>
-        { articles.map((articles)=>(
-            <li key={articles.id}>
-                title:{articles.title}
-            </li>
+        {articles.map((articles) => (
+          <li key={articles.id}>
+            title:{articles.title}
+          </li>
         ))}
       </ul>
     </div>
---
+++ src/app/rsc/page.tsx
@@ -0,0 +1,8 @@
+export default function ServerComponent() {
+    console.log('Server')
+    return (
+        <div>
+            サーバー
+        </div>
+    )
+}
